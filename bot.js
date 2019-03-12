32dc46035e0df5eaae864f0564410b1adaba6b67
Mamas159 <35382130+Mamas159@users.noreply.github.com>
2019-03-12 06:34:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,9 +88,7 @@ client.on('message', async msg => {
                     .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
  
-                    .setFooter("By JS Mehdi_..!#5041
-EMAIL
-")
+                    .setFooter("By FBIGamer")
                     msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
                    
                     // eslint-disable-next-line max-depth
