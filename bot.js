8faa46b66857ea8d01af0e8a39224dcf87d86199
Nilyass <47905754+Nilyass@users.noreply.github.com>
2019-05-14 21:37:17 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const Util = require('discord.js');
 
 client.on('ready', function(){
     var ms = 10000 ;
-    var setGame = ['single 😥'];
+    var setGame = ['Fire , Rewards'];
     var i = -1;
     var j = 0;
     setInterval(function (){ 
